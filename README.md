834819c3b1c4b0e539e54b807148b9d3ede7c84a
bartdob <dobry1@protonmail.com>
2022-06-20 13:25:43 +0200

    testing geolocation

---
+++ README.md
@@ -0,0 +1,5 @@
+[![Tests](https://github.com/bartdob/geoApp/actions/workflows/testing.yml/badge.svg)](https://github.com/bartdob/geoApp/actions/workflows/testing.yml)
+
+geoApp
+
+app to store location IP this is a backend to geo-app-geact, im using DRF and Django, and JWT TOken
---
+++ backend/tests/test_delete_geolocation.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import User
+from backend.models import GeoLocation
+import pytest
+
+
+# @pytest.mark.skip('not now')
+@pytest.mark.filterwarnings
+@pytest.mark.django_db
+def test_geoloaction_create():
+    user1 = User.objects.create_user('test', 'test@test.pl', 'test')
+    # user1.save()
+    Geo1 = GeoLocation.objects.create(ipLocation='test', userLocations=user1, name='test')
+    Geo2 = GeoLocation.objects.create(ipLocation='test2', userLocations=user1, name='test')
+    Geo3 = GeoLocation.objects.create(ipLocation='test3', userLocations=user1, name='test')
+    Geo1.delete()
+    Geo2.delete()
+    assert GeoLocation.objects.count() == 1
